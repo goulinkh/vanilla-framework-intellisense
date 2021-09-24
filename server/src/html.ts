4f3f49a4f626827a83b9738bdaeb5066bc8e9394
Goulin Khoge <goulin.khoge@gmail.com>
2021-09-24 11:50:32 +0200

    Provide autocompletion only when typing inside a class

--- server/src/html.ts
+++ server/src/html.ts
@@ -5,8 +5,58 @@ export type CompletionItems = {
   highScoreItems: ClassName[];
   normalItems: ClassName[];
 };
+
+export type HTMLElementContext = {
+  element: HTMLElement;
+  position: {
+    line: number;
+    characterToLine: number;
+    character: number;
+  };
+  text: string;
+};
 export class HTMLAutoCompletion {
   constructor(private vfService: VanillaFramework) {}
+  // The content of a text document has changed. This event is emitted
+  findHtmlNodeFromRawText(
+    text: { position: { line: number; character: number } },
+    rawContent: string,
+    document: HTMLElement
+  ): HTMLElementContext | null {
+    const characterPosition =
+      rawContent.split("\n").slice(0, text.position.line).join("\n").length +
+      text.position.character;
+    let deepestMatchingNode: HTMLElement | null = null;
+    let reachedTheBottom = false;
+    while (!reachedTheBottom) {
+      // Check if the node is in the range and is of type HtmlElement (filter the TextNodes)
+      const node: HTMLElement | undefined = (
+        deepestMatchingNode || document
+      ).childNodes.filter(
+        (node) =>
+          node.nodeType === 1 &&
+          node.range[0] <= characterPosition &&
+          node.range[1] >= characterPosition
+      )[0] as HTMLElement;
+
+      if (!node) {
+        reachedTheBottom = true;
+      } else {
+        deepestMatchingNode = node;
+      }
+    }
+    if (deepestMatchingNode)
+      return {
+        element: deepestMatchingNode,
+        position: {
+          line: text.position.line,
+          characterToLine: text.position.character,
+          character: characterPosition,
+        },
+        text: rawContent,
+      };
+    else return null;
+  }
 
   getAvailableClasses(element: HTMLElement): CompletionItems {
     const result: CompletionItems = { highScoreItems: [], normalItems: [] };
@@ -19,7 +69,9 @@ export class HTMLAutoCompletion {
       result.highScoreItems.push(
         ...Array.from(parentNode.classList.values()).flatMap((className) =>
           classTrees.flatMap((ct) =>
-            ct.classes.filter((c) => c.name.match(new RegExp(`^${className}.+`)))
+            ct.classes.filter((c) =>
+              c.name.match(new RegExp(`^${className}.+`))
+            )
           )
         )
       );
@@ -57,5 +109,18 @@ export class HTMLAutoCompletion {
     });
 
     return result;
+  }
+
+  isInsideClassValueField(context: HTMLElementContext): boolean {
+    const str = context.text.slice(
+      context.position.character - 500,
+      context.position.character + 1
+    );
+    console.log(`str`, str);
+    return (
+      !!str.match(
+        /(?:\s|:|\()(?:class(?:Name)?|\[ngClass\])\s*=\s*['"`][^'"`]*$/i
+      ) || !!str.match(/className\s*=\s*{[^}]*$/i)
+    );
   }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -87,53 +87,18 @@ connection.onInitialized((params: InitializedParams) => {
   }
 });
 
-// Cache the settings of all open documents
-
-// Only keep settings for open documents
-
-// The content of a text document has changed. This event is emitted
-function findHtmlNodeFromRawText(
-  text: { position: { line: number; character: number } },
-  rawContent: string,
-  document: HTMLElement
-): HTMLElement | null {
-  const characterPosition =
-    rawContent.split("\n").slice(0, text.position.line).join("\n").length +
-    text.position.character;
-  let deepestMatchingNode: HTMLElement | null = null;
-  let reachedTheBottom = false;
-  while (!reachedTheBottom) {
-    // Check if the node is in the range and is of type HtmlElement (filter the TextNodes)
-    const node: HTMLElement | undefined = (
-      deepestMatchingNode || document
-    ).childNodes.filter(
-      (node) =>
-        node.nodeType === 1 &&
-        node.range[0] <= characterPosition &&
-        node.range[1] >= characterPosition
-    )[0] as HTMLElement;
-
-    if (!node) {
-      reachedTheBottom = true;
-    } else {
-      deepestMatchingNode = node;
-    }
-  }
-  return deepestMatchingNode;
-}
-
 // This handler provides the initial list of the completion items.
 connection.onCompletion((params: CompletionParams): CompletionItem[] => {
   const content = documents.get(params.textDocument.uri);
   if (content?.languageId === "html") {
     const parsedContent = parseHtml(content.getText());
-    const element = findHtmlNodeFromRawText(
+    const context = html.findHtmlNodeFromRawText(
       params,
       content.getText(),
       parsedContent
     );
-    if (element && vf.isLoaded) {
-      const items = html.getAvailableClasses(element);
+    if (context && vf.isLoaded && html.isInsideClassValueField(context)) {
+      const items = html.getAvailableClasses(context.element);
       return [
         ...new Set([
           ...items.highScoreItems.map((i) => i.name),
@@ -142,7 +107,7 @@ connection.onCompletion((params: CompletionParams): CompletionItem[] => {
             tree.classes.map((c) => c.name).filter((c) => !c.match(/^.+__.+$/))
           ) || []),
         ]),
-      ].map((i) => ({ label: i, kind: CompletionItemKind.Enum }));
+      ].map((i) => ({ label: i, kind: CompletionItemKind.EnumMember }));
     }
   }
 
